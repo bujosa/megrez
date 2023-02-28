db47420079396e3f5909c8f7aface84362d3e5a2
bujosa <davidbujosa@gmail.com>
2023-02-28 19:20:47 -0400

    feat: complete logic on the server related with verify proof of the name

--- client/index.js
+++ client/index.js
@@ -5,11 +5,11 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const data = {
+    name: 'Anna Stehr',
+  };
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, data);
 
   console.log({ gift });
 }
--- server/index.js
+++ server/index.js
@@ -1,28 +1,30 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
-
-const port = 1225;
+const bodyParser = require('body-parser');
+const niceList = require('../utils/niceList');
+const MerkleTree = require('../utils/MerkleTree');
+const merkleTree = new MerkleTree(niceList);
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = merkleTree.getRoot();
 
 app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
+  const { name } = req.body;
+  const index = niceList.findIndex((n) => n === name);
+  const proof = merkleTree.getProof(index);
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
-  }
-  else {
-    res.send("You are not on the list :(");
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+
+  if (isInTheList) {
+    res.send('You got a toy robot!');
+  } else {
+    res.send('You are not on the list :(');
   }
 });
+
+const port = 1225;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
